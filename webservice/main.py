4c73347f6d2b9ad4c9228e63fee893908a0c70b1
pmontens <pieterjan@montens.net>
2019-11-13 14:16:30 +0100

    model of output, started implementing server process

--- webservice/main.py
+++ webservice/main.py
@@ -1,14 +1,55 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from model_output import Model as ResponseModel
+from model_input import Model as QueryModel
+from matching import lib_match
+import logging
+import argparse
+import yaml
+import os
+
+
+# ################################################### SETUP AND ARGUMENT PARSING
+# ##############################################################################
+def cfg_get(config=''):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    defpath = f'{current_dir}/config.default.yaml'
+    optpath = f'{current_dir}/{config}'
+    def_config_file = open(defpath, 'r')
+    opt_config_file = open(optpath, 'r') if os.path.exists(optpath) else None
+    def_config = yaml.safe_load(def_config_file)
+    config = {} if not opt_config_file else yaml.safe_load(opt_config_file)
+    return {**def_config, **config}
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.getLevelName('INFO'))
+logger.addHandler(logging.StreamHandler())
+
+parser = argparse.ArgumentParser(description='Matching server process')
+parser.add_argument('--config', dest='config', description='config file', default='')
+parser.add_argument('--debug', dest='debug', action='store_true', default=False, description='Debug mode')
+args = parser.parse_args()
+
+if args.debug:
+    logger.setLevel(logging.getLevelName('DEBUG'))
+    logger.debug('Debug activated')
+    logger.debug('Arguments: %s', args)
 
 app = FastAPI()
+config = cfg_get(args.config)
+logger.debug('Config values: \n%s', yaml.dump(config))
+
+# ################################################################ MATCHING FLOW
+# ##############################################################################
 
 
+# ################################################################ SERVER ROUTES
+# ##############################################################################
 @app.get("/")
 def root():
     return {"Salut le monde !"}
 
 
-@app.get("/match")
-def matching(q: str = None):
+@app.post("/match", response_model=ResponseModel)
+async def matching(query: QueryModel):
     return {"coucou"}
--- webservice/model_input.py
+++ webservice/model_input.py
@@ -9,6 +9,37 @@ from pydantic import (
     Schema,
 )
 
+"""
+Modèle des données en entrée pour le service matching
+
+Prototype convenu:
+```json
+{
+    '_v': 1,
+    '_timestamp': 1239120938,
+    '_query_id': 'efb7afcf-836b-4029-a3ce-f7c0b4b3499b',
+    '_session_id': 'efb7afcf-836b-4029-a3ce-f7c0b4b3499b',
+    'address': {
+        'type': 'string|geoapigouv'
+        'content': '',
+    },
+    'criteria': [
+        {
+            'id': 'distance',
+            'value': 10,
+            'priority': 5,
+        },
+        {
+            'id': 'rome_codes',
+            'value':[ { 'code': 'M1805', 'priority': 3 } ],
+            'priority': 5,
+            ]
+        },
+    ]
+}
+```
+"""
+
 
 class AddressTypes(str, Enum):
     string = 'string'
@@ -33,11 +64,11 @@ class Criteria(BaseModel):
 
 class Model(BaseModel):
     """
-    Modèle de validation des requêtes en entrée de l'outil de matching
+    Modèle de validation des requêtes de l'outil de matching
     """
     v: PositiveInt = Schema(..., alias='_v', description="Version")
     timestamp: datetime = Schema(..., alias='_timestamp', description="Timestamp (UNIX Epoch)")
     query_id: uuid.UUID = Schema(..., alias='_query_id', description="query UUID")
     session_id: uuid.UUID = Schema(..., alias='_session_id', description="browser session UUID")
     address: Address = Schema(..., description="query base address")
-    criteria: List[Criteria] = []
+    criteria: List[Criteria] = Schema([], description="List of criteria")
