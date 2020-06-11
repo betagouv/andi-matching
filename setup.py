9fbd9ae1322e6ec610f999cfadebe1aa780fb54d
Gilles Lenfant <gilles.lenfant@gmail.com>
2020-06-11 09:33:54 +0200

    Refactorisation setuptools (WIP)

--- setup.py
+++ setup.py
@@ -1,23 +1,6 @@
-from setuptools import find_packages, setup
-
-EXTRAS = {}
-
-
-def read(fname):
-    with open(fname) as fp:
-        content = fp.read()
-    return content
+from setuptools import setup
 
 
 setup(
-    name='andi_matching',
-    version='1.2.0',
-    description='ANDi Matching Toolset and Service',
-    long_description=read('README.md'),
-    extras_require=EXTRAS,
-    packages=find_packages(),
-    install_requires=[],
-    python_requires='>=3.7',
-    author='Pieterjan Montens',
-    author_email='pieterjan@montens.net',
+    # All is in ./setup.cfg
 )
